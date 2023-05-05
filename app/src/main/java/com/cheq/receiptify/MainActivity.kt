89b31c8e479b9bc0442fe9ee09f0c2b21d9f5016
Niaz Ahmed <itsniaz@gmail.com>
2023-05-05 19:27:12 +0600

    Added Gratuity in Customer Receipt

--- app/src/main/java/com/cheq/receiptify/MainActivity.kt
+++ app/src/main/java/com/cheq/receiptify/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : AppCompatActivity() {
               "totalItems": "2",
               "orderNo": "K10",
               "tableNo": "234",
-              "receiptType": "merchant",
+              "receiptType": "customer",
               "deviceType": "pos",
               "timeOfOrder": "Placed at : 01/12/2023 03:57 AM AKST",
               "paymentQRLink": "https://raw.githubusercontent.com/CHEQPlease/Receiptify/bd90ec14f119a9d767ab765d5a59057bcccabd33/app/src/main/java/com/cheq/receiptify/MainActivity.kt",
