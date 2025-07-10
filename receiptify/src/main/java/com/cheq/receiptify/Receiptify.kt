5c2e7f593c490ef6f9882a94c113684b3205c744
al-mamur-shimul <a.mamur@cheq.io>
2025-07-10 16:36:33 +0600

    PE-16169: Add vat address and vat id for tip receipt for handheld

--- receiptify/src/main/java/com/cheq/receiptify/Receiptify.kt
+++ receiptify/src/main/java/com/cheq/receiptify/Receiptify.kt
@@ -568,6 +568,14 @@ object Receiptify {
         }
 
         binding.tvBrandName.text = receiptDTO.brandName
+        binding.tvVatAddress.apply {
+            text = receiptDTO.vatAddress
+            visibility = if (receiptDTO.vatAddress.isNullOrEmpty()) View.GONE else View.VISIBLE
+        }
+        binding.tvVatId.apply {
+            text = receiptDTO.vatId
+            visibility = if (receiptDTO.vatId.isNullOrEmpty()) View.GONE else View.VISIBLE
+        }
         binding.tvOrderNo.text = "${receiptDTO.orderNo}"
         binding.tvOrderType.text = "${receiptDTO.orderType}"
 
@@ -703,6 +711,7 @@ object Receiptify {
         }
 
         binding.tvBrandName.text = receiptDTO.brandName
+
         binding.tvOrderNo.text = "${receiptDTO.orderNo}"
         binding.tvTotalItems.text =
             "${receiptDTO.totalItems}" /* TODO : Move to plural type string resource*/
